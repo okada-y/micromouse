e1d1a80f520c95603f7f2c1f1df5b208b627c835
okada-y <y.okada1629@gmail.com>
2020-02-04 00:42:42 +0900

    外部参照関数追加

--- Inc/target.h
+++ Inc/target.h
@@ -2,28 +2,37 @@
 #define TARGET_H_
 
 
+//移動方向モード
 typedef enum {
-	forward_mode 	= 0,
-	backward_mode 	= 1,
+	forward_mode 	= 0,	//前進
+	backward_mode 	= 1,	//後進
 } direction_mode;
 
+//回転方向モード
 typedef enum {
-	counter_clockwise 	= 0,
-	clockwise 	= 1,
+	counter_clockwise 	= 0,	//反時計回り
+	clockwise 	= 1,			//時計回り
 } rotation_mode;
 
-
+//加速モード
 typedef enum {
-	acceleration 	= 0, //m加速
-	deceleration 	= 1, //m減速
+	acceleration 	= 0, //加速
+	deceleration 	= 1, //減速
 } accel_mode;
 
 
 void target_1ms ( void );
+void set_direction_mode ( direction_mode );
+void set_rotation_mode ( rotation_mode );
+void set_accel_mode ( accel_mode );
+float get_target_length ( void );
+float get_ideal_length ( void );
+float get_target_angle ( void );
+float get_ideal_angle ( void );
 float get_target_move_speed ( void );
 float get_target_rotation_speed ( void );
-void set_target_length ( float set_length );
-void set_target_angle ( float set_angle );
+void set_target_length ( float );
+void set_target_angle ( float );
 void calc_target_move_accel(void);
 void calc_rotate_accel(void);
 void calc_target_move_speed(void);
