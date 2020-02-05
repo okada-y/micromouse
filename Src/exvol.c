c53d971f725f36f1ce6af77c9248637b76351d1f
y.okada <y.okada1629@gmail.com>
2020-02-05 18:22:56 +0900

    関数枠組み一部追加

--- Src/exvol.c
+++ Src/exvol.c
@@ -3,30 +3,50 @@
 
 #include <stdio.h>
 #include "main.h"
-
-static uint8_t ctrl_mode;
+#include "exvol.h"
+#include "motor.h"
+#include "control.h"
+#include "adjust.h"
 
 typedef enum {
 	trace = 0,
 	front_wall = 1,
 	side_wall = 2,
-} mode_ctrl_num;
+} ctrl_mode_num;
 
+static ctrl_mode = trace;
 
 //機能 	:メインモード処理
 //引数 	:なし
 //返り値:なし
-void set_mode_ctrl(mode_ctrl_num mode_num)
+void set_mode_ctrl(ctrl_mode_num mode_num)
 {
 	ctrl_mode = mode_num;
 }
 
 //機能 	:制御モード番号に従い、電圧を印可
-//引数 	:制御モード番号
+//引数 	:なし
 //返り値:なし
-void set_motor_vol(mode_ctrl_num mode_num)
+void set_motor_vol(void))
 {
+	switch (ctrl_mode)
+	{
+	case trace:
+		set_motor_vol_trace()
+		break;
 	
+	case front_wall:
+		set_motor_vol_front_wall()
+		break;
+	
+	case side_wall:
+		set_motor_vol_side_wall();
+		break;
+	
+	default:
+		clr_motor_vol();
+
+	}
 }
 
 //機能 	:軌跡制御時の印可電圧出力
@@ -40,7 +60,7 @@ void set_motor_vol_trace(void)
 //機能 	:前壁制御時の印可電圧出力
 //引数 	:なし
 //返り値:なし
-void set_motor_vol_front_wal(void)
+void set_motor_vol_front_wall(void)
 {
 
 }
@@ -53,24 +73,29 @@ void set_motor_vol_side_wall(void)
 
 }
 
+//機能 	:モータ印可電圧初期化
+//引数 	:なし
+//返り値:なし
+void clr_motor_vol(void)
+{
+	
+}
+
 //機能 	:各制御モードでの操作量履歴クリア
 //引数 	:制御モード番号
 //返り値:なし
 //target.c adjust.cにおいて操作履歴クリア関数を作り、呼び出すこと
-void clr_operate_history(mode_ctrl_num mode_num)
+void clr_operate_history(ctrl_mode_num mode_num)
 {
 
 }
 
-
-static uint8_t correction_mode = 0;		//m 補正モードフラグ 0:目標速度　1:前壁センサ値
-
-
 //機能 	:非線形部は使わないように、印可電圧を調整
 //引数 	:なし
 //返り値:なし
-
-
+//
+void motor_vol_adjust (void)
+{
 	/* m バッテリー電圧とモータに印加する電圧から、duty[*0.1%]を算出	*/
 	operation_amount_r = target_vol_r / (Battery_GetVoltage()) * 1000;
 	operation_amount_l = target_vol_l / (Battery_GetVoltage()) * 1000;
@@ -93,3 +118,9 @@ static uint8_t correction_mode = 0;		//m 補正モードフラグ 0:目標速度
 	/*mモータに電圧を印加 */
 	Motor_SetDuty_Left((int16_t)operation_amount_l);
 	Motor_SetDuty_Right((int16_t)operation_amount_r);
+}
+
+//機能 	:モータに印可電圧を出力
+//引数 	:なし
+//返り値:なし
+//ここでセットデューティー
